32a375ab16d72564f2cc8324c3ef972010e656e5
ChenHuan <chscls@163.com>
2017-05-27 15:00:34 +0800

    1111

--- src/components/Floors.js
+++ src/components/Floors.js
@@ -6,6 +6,7 @@ import styles from '../routes/IndexPage.css';
 
 const Floors = (props) => {
   return (
+       <div>
     <div className="j_floor floor-con" >
       <div className="floor-line-con color-type-green" id="J_FloorMLRS" style={{width:'1230px'}}>
 	<i className="color-mark"></i><div className="floor-name">川菜大师</div><div className="floor-sub-name">Sichuan &amp; Cuisine &amp;Master</div>
@@ -181,6 +182,206 @@ const Floors = (props) => {
 </div>
 </div>
     </div>
+
+
+
+<div className="j_floor floor-con" >
+      <div className="floor-line-con color-type-blue" id="J_FloorMLRS" style={{width:'1230px'}}>
+	<i className="color-mark"></i><div className="floor-name">技术大牛</div><div className="floor-sub-name">Information &amp; Technology</div>
+	<div className="j_lineBody line-body fp-content-loading" data-goldlog="/tmallfp.5108.1" data-appid="201603202,09051,201603211" style={{opacity: '1'}}>
+
+
+<div className="hot-word-con" data-spm="2016076">
+	
+	<a className="hot-word" href="">前端</a>
+	
+	<a className="hot-word" href="">云计算</a>
+	
+	<a className="hot-word" href="">大数据</a>
+	
+<a className="hot-word" href="">人工智能</a>
+</div>
+
+<div data-spm="2016077">
+
+<a className="big-banner-con" href="">
+	<img src="30.jpg"/>
+	
+		<div className="act-title-ctn">
+			
+				
+			
+				
+			
+				
+			
+		<div className="slide-item" style={{marginTop: '0px', display: 'block'}}>跟着大牛学技术 &gt;</div><div className="slide-item" style={{marginTop: '0px', display: 'block'}}>内衣外穿新时尚 &gt;</div><div className="slide-item" style={{marginTop: '0px', display: 'block'}}>大牌超值满减 &gt;</div></div>
+	
+	<div className="title-con">
+		<div className="title">知用嘉年华</div>
+		<div className="sub-title">免费学习</div>
+		<div className="decoration"></div>
+	</div>
+	
+    
+
+</a>
+
+
+<div className="middle-column-con">
+	
+		
+			<a className="grid one-grid" href="">
+				<div className="title">Android</div>
+				<div className="sub-title">星球大战</div>
+				<img src="31.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/667215758/7580091?sellerId=667215758&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=2&amp;actId=13703763&amp;acm=09051.1003.1.1105274&amp;id=7580091&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13703763.300.2.subject-1002">
+				<div className="title">Ios</div>
+				<div className="sub-title">吃一个苹果</div>
+				<img src="32.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/822320219/7669972?sellerId=822320219&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=3&amp;actId=13840403&amp;acm=09051.1003.1.1105274&amp;id=7669972&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13840403.300.2.subject-1008">
+				<div className="title">Java</div>
+				<div className="sub-title">喝一杯咖啡</div>
+				<img src="33.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/714384985/7705264?sellerId=714384985&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=4&amp;actId=13894875&amp;acm=09051.1003.1.1105274&amp;id=7705264&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13894875.300.2.subject-1002">
+				<div className="title">Docker</div>
+				<div className="sub-title">轮渡</div>
+				<img src="34.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/2316182950/7602364?sellerId=2316182950&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=5&amp;actId=13737834&amp;acm=09051.1003.1.1105274&amp;id=7602364&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13737834.300.2.subject-1009">
+				<div className="title">Openstack</div>
+				<div className="sub-title">站在云端</div>
+				<img src="35.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/92438150/7688006?sellerId=92438150&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=6&amp;actId=13868109&amp;acm=09051.1003.1.1105274&amp;id=7688006&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13868109.300.2.subject-1009">
+				<div className="title">React</div>
+				<div className="sub-title">声明式设计、高效、灵活</div>
+				<img src="36.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+	
+		
+	
+		
+	
+</div>
+<div className="right-column-con">
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Hadoop</div>
+				<div className="sub-title">一只大象，一个巨人</div>
+				<img src="37.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Unity3d</div>
+				<div className="sub-title">游戏人生</div>
+				<img src="38.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Photoshop</div>
+				<div className="sub-title">艺术人生</div>
+				<img src="39.jpg"/>
+				
+    
+
+			</a>
+		
+	
+</div>
+
+
+
+
+
+</div>
+</div>
+</div>
+    </div>
+
+
+
+    	<div data-spm="2016901" style={{marginTop: '10px'}} className="tanx-con-mm_12852562_1778064_37802121 full-banner clearfix j_tanxBanner j_LazyloadCon fp-content-loading j_exposureCon" data-inited="1"><a href="https://click.mz.simba.taobao.com/ecpm?e=FfW1i%2Bx4U001coovOinjSD6gEdXn0VYUcu7lTK3qqzUw%2FZSp%2FO%2FfM7%2BLt2khHsChClQupYKoz4a7GsveaVM4lgtIBuaU4rc8UK7RDtIZydUyXElRAMLwo9OMT%2BjBtNp7%2FMunkswZxss3raQopFwc8EIQgPjj8GCr7I1RE%2BA%2BExJyD7uMQ%2BGVAs3eQZDe8L65r6eYcEHsY6lhsyUzvE7KQbLDU22ROm4fHu3yN3re1vWmETCSJyV1pb6x0hxvL1XsqpqtvE5BYkbKBgvtWhOvg7sEeYkLQ%2BD8EboYhmR1FsuzChHIS4d58BgoqG7QlwxqKkrZ6%2BlcbrMbeEC0GJrdOBW0kLmjjWUHXzw4mcHNcwoq0eFFS53N5P6SxR1g51pYEG0a8sdIX7foLX8X5k6oSQ%3D%3D&amp;u=https%3A%2F%2Fmochagushi.tmall.com%2Fshop%2Fview_shop.htm%3Fmytmenu%3Dmdianpu%26utkn%3Dg%252Cysulf2fzzlfmfrxmxwrll2q1493552045301%26user_number_id%3D377636605%26scm%3D1028.1.1.20001&amp;k=409">
+    <img src="12.jpg" width="1620" height="90"/></a><a href="">
+    <img src="12.jpg" width="1620" height="90"/></a>
+		<ins style={{display:'inline-block',padding:'0',margin:'0',width:'1620px',height:'90px',zoom:'1',position:'relative'}} id="tanxssp-outer-conmm_12852562_1778064_37802121">
+            <div id="tanxssp_con_mm_12852562_1778064_37802121" style={{overflow:'hidden',display:'inline-block',position:'relative',width:'1620px',height:'90px',zoom:'1',font:"12px/1.5 tahoma,'Hiragino Sans GB','microsoft yahei',sans-serif"}}>
+        <a href="" target="_blank">
+        <img  src="12.jpg" width="1620px" height="90px"/></a>
+        </div>
+        </ins>
+        <a className="j_tanxItem" id="tanx-a-mm_12852562_1778064_37802121" style={{display: 'none'}}>
+		</a>
+	</div>
+    </div>
+
+
+
+
+
+
   );
 };
 
