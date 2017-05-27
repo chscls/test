2314439052069578a5b5783ad7fc143d932d7aa8
ChenHuan <chscls@163.com>
2017-05-27 15:18:41 +0800

    1111

--- src/components/Floors.js
+++ src/components/Floors.js
@@ -8,7 +8,7 @@ const Floors = (props) => {
   return (
        <div>
     <div className="j_floor floor-con" >
-      <div className="floor-line-con color-type-green" id="J_FloorMLRS" style={{width:'1230px'}}>
+      <div className="floor-line-con color-type-red" id="J_FloorMLRS" style={{width:'1230px'}}>
 	<i className="color-mark"></i><div className="floor-name">川菜大师</div><div className="floor-sub-name">Sichuan &amp; Cuisine &amp;Master</div>
 	<div className="j_lineBody line-body fp-content-loading" data-goldlog="/tmallfp.5108.1" data-appid="201603202,09051,201603211" style={{opacity: '1'}}>
 
@@ -375,7 +375,598 @@ const Floors = (props) => {
         <a className="j_tanxItem" id="tanx-a-mm_12852562_1778064_37802121" style={{display: 'none'}}>
 		</a>
 	</div>
+
+
+
+
+
+
+
+<div className="j_floor floor-con" >
+      <div className="floor-line-con color-type-pink" id="J_FloorMLRS" style={{width:'1230px'}}>
+	<i className="color-mark"></i><div className="floor-name">医学护理</div><div className="floor-sub-name">Information &amp; Technology</div>
+	<div className="j_lineBody line-body fp-content-loading" data-goldlog="/tmallfp.5108.1" data-appid="201603202,09051,201603211" style={{opacity: '1'}}>
+
+
+<div className="hot-word-con" data-spm="2016076">
+	
+	<a className="hot-word" href="">口腔</a>
+	
+	<a className="hot-word" href="">器械</a>
+	
+	<a className="hot-word" href="">护理</a>
+	
+<a className="hot-word" href="">药材</a>
+</div>
+
+<div data-spm="2016077">
+
+<a className="big-banner-con" href="">
+	<img src="30.jpg"/>
+	
+		<div className="act-title-ctn">
+			
+				
+			
+				
+			
+				
+			
+		<div className="slide-item" style={{marginTop: '0px', display: 'block'}}>跟着大牛学技术 &gt;</div><div className="slide-item" style={{marginTop: '0px', display: 'block'}}>内衣外穿新时尚 &gt;</div><div className="slide-item" style={{marginTop: '0px', display: 'block'}}>大牌超值满减 &gt;</div></div>
+	
+	<div className="title-con">
+		<div className="title">知用嘉年华</div>
+		<div className="sub-title">免费学习</div>
+		<div className="decoration"></div>
+	</div>
+	
+    
+
+</a>
+
+
+<div className="middle-column-con">
+	
+		
+			<a className="grid one-grid" href="">
+				<div className="title">Android</div>
+				<div className="sub-title">星球大战</div>
+				<img src="31.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/667215758/7580091?sellerId=667215758&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=2&amp;actId=13703763&amp;acm=09051.1003.1.1105274&amp;id=7580091&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13703763.300.2.subject-1002">
+				<div className="title">Ios</div>
+				<div className="sub-title">吃一个苹果</div>
+				<img src="32.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/822320219/7669972?sellerId=822320219&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=3&amp;actId=13840403&amp;acm=09051.1003.1.1105274&amp;id=7669972&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13840403.300.2.subject-1008">
+				<div className="title">Java</div>
+				<div className="sub-title">喝一杯咖啡</div>
+				<img src="33.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/714384985/7705264?sellerId=714384985&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=4&amp;actId=13894875&amp;acm=09051.1003.1.1105274&amp;id=7705264&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13894875.300.2.subject-1002">
+				<div className="title">Docker</div>
+				<div className="sub-title">轮渡</div>
+				<img src="34.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/2316182950/7602364?sellerId=2316182950&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=5&amp;actId=13737834&amp;acm=09051.1003.1.1105274&amp;id=7602364&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13737834.300.2.subject-1009">
+				<div className="title">Openstack</div>
+				<div className="sub-title">站在云端</div>
+				<img src="35.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/92438150/7688006?sellerId=92438150&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=6&amp;actId=13868109&amp;acm=09051.1003.1.1105274&amp;id=7688006&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13868109.300.2.subject-1009">
+				<div className="title">React</div>
+				<div className="sub-title">声明式设计、高效、灵活</div>
+				<img src="36.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+	
+		
+	
+		
+	
+</div>
+<div className="right-column-con">
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Hadoop</div>
+				<div className="sub-title">一只大象，一个巨人</div>
+				<img src="37.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Unity3d</div>
+				<div className="sub-title">游戏人生</div>
+				<img src="38.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Photoshop</div>
+				<div className="sub-title">艺术人生</div>
+				<img src="39.jpg"/>
+				
+    
+
+			</a>
+		
+	
+</div>
+
+
+
+
+
+</div>
+</div>
+</div>
     </div>
+
+
+
+
+
+
+
+
+
+
+
+<div className="j_floor floor-con" >
+      <div className="floor-line-con color-type-green" id="J_FloorMLRS" style={{width:'1230px'}}>
+	<i className="color-mark"></i><div className="floor-name">智慧农业</div><div className="floor-sub-name">Information &amp; Technology</div>
+	<div className="j_lineBody line-body fp-content-loading" data-goldlog="/tmallfp.5108.1" data-appid="201603202,09051,201603211" style={{opacity: '1'}}>
+
+
+<div className="hot-word-con" data-spm="2016076">
+	
+	<a className="hot-word" href="">口腔</a>
+	
+	<a className="hot-word" href="">器械</a>
+	
+	<a className="hot-word" href="">护理</a>
+	
+<a className="hot-word" href="">药材</a>
+</div>
+
+<div data-spm="2016077">
+
+<a className="big-banner-con" href="">
+	<img src="30.jpg"/>
+	
+		<div className="act-title-ctn">
+			
+				
+			
+				
+			
+				
+			
+		<div className="slide-item" style={{marginTop: '0px', display: 'block'}}>跟着大牛学技术 &gt;</div><div className="slide-item" style={{marginTop: '0px', display: 'block'}}>内衣外穿新时尚 &gt;</div><div className="slide-item" style={{marginTop: '0px', display: 'block'}}>大牌超值满减 &gt;</div></div>
+	
+	<div className="title-con">
+		<div className="title">知用嘉年华</div>
+		<div className="sub-title">免费学习</div>
+		<div className="decoration"></div>
+	</div>
+	
+    
+
+</a>
+
+
+<div className="middle-column-con">
+	
+		
+			<a className="grid one-grid" href="">
+				<div className="title">Android</div>
+				<div className="sub-title">星球大战</div>
+				<img src="31.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/667215758/7580091?sellerId=667215758&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=2&amp;actId=13703763&amp;acm=09051.1003.1.1105274&amp;id=7580091&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13703763.300.2.subject-1002">
+				<div className="title">Ios</div>
+				<div className="sub-title">吃一个苹果</div>
+				<img src="32.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/822320219/7669972?sellerId=822320219&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=3&amp;actId=13840403&amp;acm=09051.1003.1.1105274&amp;id=7669972&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13840403.300.2.subject-1008">
+				<div className="title">Java</div>
+				<div className="sub-title">喝一杯咖啡</div>
+				<img src="33.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/714384985/7705264?sellerId=714384985&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=4&amp;actId=13894875&amp;acm=09051.1003.1.1105274&amp;id=7705264&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13894875.300.2.subject-1002">
+				<div className="title">Docker</div>
+				<div className="sub-title">轮渡</div>
+				<img src="34.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/2316182950/7602364?sellerId=2316182950&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=5&amp;actId=13737834&amp;acm=09051.1003.1.1105274&amp;id=7602364&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13737834.300.2.subject-1009">
+				<div className="title">Openstack</div>
+				<div className="sub-title">站在云端</div>
+				<img src="35.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid one-grid" href="//content.tmall.com/wow/pegasus/subject/0/92438150/7688006?sellerId=92438150&amp;pvid=3bb2a9c0-7b13-473a-a7aa-1c6ff2f4439f&amp;pos=6&amp;actId=13868109&amp;acm=09051.1003.1.1105274&amp;id=7688006&amp;scm=1007.13306.32861.100200300000000&amp;gccpm=13868109.300.2.subject-1009">
+				<div className="title">React</div>
+				<div className="sub-title">声明式设计、高效、灵活</div>
+				<img src="36.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+	
+		
+	
+		
+	
+</div>
+<div className="right-column-con">
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Hadoop</div>
+				<div className="sub-title">一只大象，一个巨人</div>
+				<img src="37.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Unity3d</div>
+				<div className="sub-title">游戏人生</div>
+				<img src="38.jpg"/>
+				
+    
+
+			</a>
+		
+	
+		
+			<a className="grid second-grid" href="">
+				<div className="title">Photoshop</div>
+				<div className="sub-title">艺术人生</div>
+				<img src="39.jpg"/>
+				
+    
+
+			</a>
+		
+	
+</div>
+
+
+
+
+
+</div>
+</div>
+</div>
+    </div>
+
+
+
+
+
+
+
+
+
+	<div data-spm="2016901" style={{marginTop: '10px'}} className="tanx-con-mm_12852562_1778064_37802121 full-banner clearfix j_tanxBanner j_LazyloadCon fp-content-loading j_exposureCon" data-inited="1"><a href="https://click.mz.simba.taobao.com/ecpm?e=FfW1i%2Bx4U001coovOinjSD6gEdXn0VYUcu7lTK3qqzUw%2FZSp%2FO%2FfM7%2BLt2khHsChClQupYKoz4a7GsveaVM4lgtIBuaU4rc8UK7RDtIZydUyXElRAMLwo9OMT%2BjBtNp7%2FMunkswZxss3raQopFwc8EIQgPjj8GCr7I1RE%2BA%2BExJyD7uMQ%2BGVAs3eQZDe8L65r6eYcEHsY6lhsyUzvE7KQbLDU22ROm4fHu3yN3re1vWmETCSJyV1pb6x0hxvL1XsqpqtvE5BYkbKBgvtWhOvg7sEeYkLQ%2BD8EboYhmR1FsuzChHIS4d58BgoqG7QlwxqKkrZ6%2BlcbrMbeEC0GJrdOBW0kLmjjWUHXzw4mcHNcwoq0eFFS53N5P6SxR1g51pYEG0a8sdIX7foLX8X5k6oSQ%3D%3D&amp;u=https%3A%2F%2Fmochagushi.tmall.com%2Fshop%2Fview_shop.htm%3Fmytmenu%3Dmdianpu%26utkn%3Dg%252Cysulf2fzzlfmfrxmxwrll2q1493552045301%26user_number_id%3D377636605%26scm%3D1028.1.1.20001&amp;k=409">
+    <img src="12.jpg" width="1620" height="90"/></a><a href="">
+    <img src="12.jpg" width="1620" height="90"/></a>
+		<ins style={{display:'inline-block',padding:'0',margin:'0',width:'1620px',height:'90px',zoom:'1',position:'relative'}} id="tanxssp-outer-conmm_12852562_1778064_37802121">
+            <div id="tanxssp_con_mm_12852562_1778064_37802121" style={{overflow:'hidden',display:'inline-block',position:'relative',width:'1620px',height:'90px',zoom:'1',font:"12px/1.5 tahoma,'Hiragino Sans GB','microsoft yahei',sans-serif"}}>
+        <a href="" target="_blank">
+        <img  src="12.jpg" width="1620px" height="90px"/></a>
+        </div>
+        </ins>
+        <a className="j_tanxItem" id="tanx-a-mm_12852562_1778064_37802121" style={{display: 'none'}}>
+		</a>
+	</div>
+
+
+
+
+
+
+
+
+<div className="j_wonderful wonderful-con mui-lift-cur-pannel" id="J_FloorCNXH" style={{width:'1230px'}}> 
+
+<h2 className="wonderful-title">
+        <span className="text-ctn">
+            <i className="fp-iconfont nav-item-icon icon"></i>猜你喜欢
+        </span>
+    </h2>
+<div className="module-content j_wonderfulList" style={{width:'1230px'}}>
+
+<div data-spm="20161011">
+
+
+<ul className="wonderful-line  j_exposureExtra1 ">
+    
+    
+    <li className="wonderful-item ">
+        <a className="card-item  " href="//detail.tmall.com/item.htm?abtest=_AB-LR845-PR845&amp;pvid=8279c180-44f1-448b-a2da-c4522cbe0d13&amp;pos=1&amp;abbucket=_AB-M845_B0&amp;acm=201509290.1003.1.1286473&amp;id=547118984948&amp;scm=1007.12710.81710.100200300000000 ">
+            <span className="item-pic">
+            <img src="//img.alicdn.com/bao/uploaded/i3/2616970884/TB2PLT.khxmpuFjSZFNXXXrRXXa_!!2616970884.jpg_400x400q60.jpg" width="100%"/>
+                </span>
+                <span className="item-info">
+                    <span className="item-desc">
+                        <em className="item-name" title="【限时限量抢】Apple/苹果 iPhone 7 Plus 128G 红色全网通手机">【限时限量抢】Apple/苹果 iPhone 7 Plus 128G 红色全网通手机</em>
+                    </span>
+                    <span className="item-detail">
+                        <span className="item-price">
+
+
+
+<span className="mui-price  ">
+    <i className="mui-price-rmb">¥</i>
+    <span className="mui-price-integer">6688</span>
+    
+    <span className="mui-price-decimal">.0</span>
+    
+</span>
+
+</span>
+                        
+                    </span>
+                </span>
+        </a>
+    </li>
+    
+    
+
+
+    
+    <li className="wonderful-item ">
+        <a className="card-item  " href="//detail.tmall.com/item.htm?abtest=_AB-LR845-PR845&amp;pvid=8279c180-44f1-448b-a2da-c4522cbe0d13&amp;pos=2&amp;abbucket=_AB-M845_B0&amp;acm=201509290.1003.1.1286473&amp;id=544913594159&amp;scm=1007.12710.81710.100200300000000 ">
+            <span className="item-pic">
+            <img src="//img.alicdn.com/bao/uploaded/bao/upload/TB1jGI6RXXXXXbaaXXXwu0bFXXX.png_400x400q60.jpg" width="100%"/>
+                </span>
+                <span className="item-info">
+                    <span className="item-desc">
+                        <em className="item-name" title="科沃斯地宝DD35扫地机器人吸尘器智能家用超薄全自动洗擦地机拖地">科沃斯地宝DD35扫地机器人吸尘器智能家用超薄全自动洗擦地机拖地</em>
+                    </span>
+                    <span className="item-detail">
+                        <span className="item-price">
+
+
+
+<span className="mui-price  ">
+    <i className="mui-price-rmb">¥</i>
+    <span className="mui-price-integer">2199</span>
+    
+    <span className="mui-price-decimal">.0</span>
+    
+</span>
+
+</span>
+                        
+                    </span>
+                </span>
+        </a>
+    </li>
+    
+    
+
+
+    
+    <li className="wonderful-item ">
+        <a className="card-item  " href="//detail.tmall.com/item.htm?abtest=_AB-LR845-PR845&amp;pvid=8279c180-44f1-448b-a2da-c4522cbe0d13&amp;pos=3&amp;abbucket=_AB-M845_B0&amp;acm=201509290.1003.1.1286473&amp;id=20170473600&amp;scm=1007.12710.81710.100200300000000 ">
+            <span className="item-pic">
+            <img src="//img.alicdn.com/bao/uploaded/i1/430490406/TB2bKD2f_cCL1FjSZFPXXXZgpXa_!!430490406.jpg_400x400q60.jpg" width="100%"/>
+                </span>
+                <span className="item-info">
+                    <span className="item-desc">
+                        <em className="item-name" title="全棉时代 婴儿纯棉柔巾 新生儿干湿两用 宝宝可湿纸巾 加厚18包">全棉时代 婴儿纯棉柔巾 新生儿干湿两用 宝宝可湿纸巾 加厚18包</em>
+                    </span>
+                    <span className="item-detail">
+                        <span className="item-price">
+
+
+
+<span className="mui-price  ">
+    <i className="mui-price-rmb">¥</i>
+    <span className="mui-price-integer">324</span>
+    
+    <span className="mui-price-decimal">.0</span>
+    
+</span>
+
+</span>
+                        
+                    </span>
+                </span>
+        </a>
+    </li>
+    
+    
+
+
+    
+        <li className="shop-in-wonder">
+    
+    <a className="img-ctn" href="//a.m.taobao.com/i542477274329.htm?itemId=542477274329&amp;sellerId=1045424866&amp;pvid=3f9624ed-0434-4668-bae0-3747d08ce933&amp;scm=1007.13088.38695.100200300000000&amp;itemClk=">
+        <img className="shop-item-pic" src="//gw.alicdn.com/bao/uploaded/TB1j2yfOFXXXXaEaXXXXXXXXXXX_!!0-item_pic.jpg"/>
+        <div className="black-mask"></div>
+
+    </a>
+    
+    <a className="meow-head" href="//shop100192812.taobao.com/?sellerPos=1&amp;abtest=_AB-LR1157-PR1157&amp;sellerId=1045424866&amp;pvid=3f9624ed-0434-4668-bae0-3747d08ce933&amp;pos=1&amp;abbucket=_AB-M1157_B0&amp;shopClk=&amp;acm=09041.1003.1.966381&amp;scm=1003.1.09041.SHOP_100192812_966381&amp;sellerType=0">
+        
+        
+        <div className="shop-logo-wrapper">
+            <img className="shop-logo" src="//gw.alicdn.com/bao/uploaded/i1/TB16dcWMpXXXXcPXXXXSutbFXXX.jpg"/>
+        </div>
+    </a>
+    <a className="shop-name" href="//shop100192812.taobao.com/?sellerPos=1&amp;abtest=_AB-LR1157-PR1157&amp;sellerId=1045424866&amp;pvid=3f9624ed-0434-4668-bae0-3747d08ce933&amp;pos=1&amp;abbucket=_AB-M1157_B0&amp;shopClk=&amp;acm=09041.1003.1.966381&amp;scm=1003.1.09041.SHOP_100192812_966381&amp;sellerType=0">MINI天猫官方旗舰店</a>
+    <p className="shop-fans">4.9 万粉丝</p>
+</li>
+
+    
+    
+
+
+    
+    <li className="wonderful-item  last">
+        <a className="card-item   last-1920" href="//detail.tmall.com/item.htm?abtest=_AB-LR845-PR845&amp;pvid=8279c180-44f1-448b-a2da-c4522cbe0d13&amp;pos=4&amp;abbucket=_AB-M845_B0&amp;acm=201509290.1003.1.1286473&amp;id=522680881881&amp;scm=1007.12710.81710.100200300000000 ">
+            <span className="item-pic">
+            <img src="//img.alicdn.com/bao/uploaded/bao/upload/TB1iRV1OVXXXXXLXpXXwu0bFXXX.png_400x400q60.jpg" width="100%"/>
+                </span>
+                <span className="item-info">
+                    <span className="item-desc">
+                        <em className="item-name" title="Kindle官方旗舰店Kindle Paperwhite3亚马逊电子书阅读器电纸书">Kindle官方旗舰店Kindle Paperwhite3亚马逊电子书阅读器电纸书</em>
+                    </span>
+                    <span className="item-detail">
+                        <span className="item-price">
+
+
+
+<span className="mui-price  ">
+    <i className="mui-price-rmb">¥</i>
+    <span className="mui-price-integer">958</span>
+    
+    <span className="mui-price-decimal">.0</span>
+    
+</span>
+
+</span>
+                        
+                    </span>
+                </span>
+        </a>
+    </li>
+    
+    
+</ul>
+
+
+
+
+
+
+
+	</div>
+
+	</div>
+
+
+
+	<div className="tm-end show"></div>
+<div className="j_end"></div>
+
+</div>
+
+
+
+    </div>
+
+
+
+
+
 
 
 
