9327b46f7ea605904a622d1e9a3ca7f52a78f592
chscls <chscls@163.com>
2017-05-31 22:18:05 +0800

    111111

--- src/components/IndexContent.js
+++ src/components/IndexContent.js
@@ -6,14 +6,41 @@ import Activity2 from '../components/Activity2'
 import { Carousel } from 'antd'
 import Nav from '../components/Nav'
 class IndexContent extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
+    this.state = {
+      detailShow: 'none',
+    }
   }
 
-  render () {
+  handleMouseOver = (e) => {
+    this.setState({
+      detailShow: 'block',
+
+    })
+   var y = e.currentTarget;
+    var x= y.parentNode.childNodes;
+    
+     
+   
+    
+ for(var i=0;i<x.length;i++){
+x[i].style.backgroundColor = "rgba(238, 238, 238, 0.952941)";
+}
+ y.style.backgroundColor = "white";
+  }
+   
+  handleMouseLeave = (e) => {
+    this.setState({
+      detailShow: 'none'
+    })
+
+  }
+  
+  render() {
     return (
       <div>
-        <Nav/>
+        <Nav />
         <div id='content'>
           <div className='main-nav' data-spm='2016004'>
             <div className='inner-con0'>
@@ -35,11 +62,11 @@ class IndexContent extends React.Component {
               </div>
             </div>
           </div>
-          <div className='j_category category-con'>
+          <div className='j_category category-con' onMouseLeave={this.handleMouseLeave}>
             <div className='category-inner-con j_categoryInnerCon j_categoryTab'>
               <div className='j_logo logo-con' data-spm='2015002'>
                 <div className='doodle-con j_doodleCon'>
-                  <a className='j_doodleLink' style={{display: 'block',height: '100%',width: '100%'}} href=''><img className='doodle hide j_doodle' src='123.gif' data-src='123.gif' /></a>
+                  <a className='j_doodleLink' style={{ display: 'block', height: '100%', width: '100%' }} href=''><img className='doodle hide j_doodle' src='123.gif' data-src='123.gif' /></a>
                 </div>
               </div>
               <div className='category-type'>
@@ -48,96 +75,115 @@ class IndexContent extends React.Component {
                   课程分类
                 </div>
               </div>
-              <div className='category-tab-content'>
+              <div className='category-tab-content'  >
                 <div className='nav-con j_tabPannel category-tab-pannel pannel-normal'>
                   <ul className='normal-nav clearfix'>
-                    <li className='j_MenuNav nav-item nav-item-0 category-loaded' data-spm='category2016010'>
+                    <li className='j_MenuNav nav-item nav-item-0 category-loaded' data-spm='category2016010' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 农林牧渔
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 农林牧渔
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 交通运输
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 交通运输
                       </Link>
                       <b className='arrow'></b>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 生化药品
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 生化药品
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 资源开发测绘
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 资源开发测绘
                       </Link>
                       <Link className='arrow'></Link>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link style={{color: 'black'}} href=''> 材料能源
+                      <Link style={{ color: 'black' }}
+                        href=''> 材料能源
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 土建
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 土建
                       </Link>
                       <b className='arrow'></b>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 水利
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 水利
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 制造
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 制造
                       </Link>
                       <b className='arrow'></b>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 电子信息
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 电子信息
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 环保气象安全
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 环保气象安全
                       </Link>
                       <b className='arrow'></b>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 轻纺食品
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 轻纺食品
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 财经
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 财经
                       </Link>
                       <b className='arrow'></b>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 医药卫生
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 医药卫生
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 旅游
+                      <Link to='Search' style={{ color: 'black' }} href=''> 旅游
                       </Link>
                       <b className='arrow'></b>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 公共事业
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 公共事业
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 文化教育
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 文化教育
                       </Link>
                       <b className='arrow'></b>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 艺术设计传媒
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 艺术设计传媒
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 公安
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 公安
                       </Link>
                       <b className='arrow'></b>
                     </li>
-                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011'>
+                    <li className='j_MenuNav nav-item nav-item-1 category-loaded' data-spm='category2016011' onMouseOver={this.handleMouseOver} >
                       <i className='fp-iconfont nav-item-icon icon'></i><i className='dot fp-iconfont'></i>
-                      <Link to='Search' style={{color: 'black'}} href=''> 法律
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 法律
                       </Link>
                       /
-                      <Link to='Search' style={{color: 'black'}} href=''> 其他
+                      <Link to='Search'
+                        style={{ color: 'black' }} href=''> 其他
                       </Link>
                       <b className='arrow'></b>
                     </li>
@@ -145,18 +191,18 @@ class IndexContent extends React.Component {
                 </div>
               </div>
               <div className='content-con j_categoryContent'>
-                <div className='pannel-con j_CategoryMenuPannel' style={{display: 'none'}}>
+                <div className='pannel-con j_CategoryMenuPannel' style={{ display: this.state.detailShow }}>
                   <div className='pannel-0'>
                     <div className='hot-word-con' data-spm='subpannel2016025'>
                       <div className='hot-word-line'>
                         <div className='line-title'>
                           <div className='title-text'>
-                            当季流行
+                            养殖业
                           </div><i className='fp-iconfont'></i>
                         </div>
                         <div className='line-con'>
-                          <a className='hot-word highlight ' href='https://list.tmall.com/search_product.htm?new=1&amp;q=%CC%EC%C3%A8%C5%AE%D7%B0&amp;pos=2&amp;style=g&amp;from=.list.pc_1_searchbutton&amp;acm=2016031463.1003.2.1398732&amp;search_condition=48&amp;sort=s&amp;shopType=any&amp;scm=1003.2.2016031463.OTHER_1496306406551_1398732'>夏季新品</a>
-                          <a className='hot-word ' href='https://list.tmall.com/search_product.htm?q=%B4%AC%CD%E0+%C5%AE&amp;spm=a220m.1000858.a2227oh.d100&amp;pos=12&amp;from=.list.pc_1_searchbutton&amp;acm=2016031463.1003.2.1398732&amp;type=p&amp;scm=1003.2.2016031463.OTHER_1493288070515_1398732'>百搭船袜</a>
+                          <Link to="Search" className='hot-word highlight ' href=''>养鸡</Link>
+                          <Link to="Search"className='hot-word ' href=''>养鸭</Link>
                           <div className='seprate clearfix'></div>
                         </div>
                       </div>
@@ -164,40 +210,40 @@ class IndexContent extends React.Component {
                   </div>
                   <div className='sub-logo-con'>
                     <div data-spm='subpannel2016026'>
-                      <a className='logo' href='https://goelia.tmall.com/shop/view_shop.htm?spm=a220m.1000862.1000730.3.3KBpcC&amp;user_number_id=133006562&amp;pos=1&amp;acm=2016022913.1003.2.709009&amp;rn=99a403f2b2463b658523cb6694775b4a&amp;scm=1003.2.2016022913.OTHER_1481821973536_709009'><img src='//img.alicdn.com/bao/uploaded/TB1B_DcJpXXXXXWXFXXSutbFXXX.jpg_170x120q30.jpg' /></a>
-                      <a className='logo' href='https://metersbonwe.tmall.com/shop/view_shop.htm?spm=a220m.1000862.1000730.3.REt9sA&amp;user_number_id=134363478&amp;pos=2&amp;acm=2016022913.1003.2.709009&amp;rn=b8a6a2d2216c7e00de44f71ed9894bcb&amp;scm=1003.2.2016022913.OTHER_1483508856009_709009'><img src='//img.alicdn.com/bao/uploaded/i4/T1gRqVXk4fXXb1upjX.jpg_170x120q30.jpg' /></a>
-                      <a className='logo' href='https://olrain.tmall.com/shop/view_shop.htm?spm=a220m.1000862.1000730.3.o3sm9O&amp;user_number_id=758392199&amp;pos=3&amp;acm=2016022913.1003.2.709009&amp;rn=ccafa3b45dc79bdc53a8f3f342c585f8&amp;scm=1003.2.2016022913.OTHER_1480239037214_709009'><img src='//img.alicdn.com/bao/uploaded/TB1wNJAHFXXXXc7XpXXSutbFXXX.jpg_170x120q30.jpg' /></a>
+                      <Link to="Search" className='logo' href=''><img src='//img.alicdn.com/bao/uploaded/TB1B_DcJpXXXXXWXFXXSutbFXXX.jpg_170x120q30.jpg' /></Link>
+                      <Link to="Search" className='logo' href=''><img src='//img.alicdn.com/bao/uploaded/i4/T1gRqVXk4fXXb1upjX.jpg_170x120q30.jpg' /></Link>
+                      <Link to="Search" className='logo' href=''><img src='//img.alicdn.com/bao/uploaded/TB1wNJAHFXXXXc7XpXXSutbFXXX.jpg_170x120q30.jpg' /></Link>
                     </div>
                     <div data-spm='subpannel2016027'>
-                      <a className='activity clearfix' href='https://nvzhuang.tmall.com/?pos=1&amp;acm=201603010.1003.2.708961&amp;scm=1003.2.201603010.OTHER_1483238608900_708961'><img src='https://img.alicdn.com/tps/TB1xnkQKVXXXXaJXXXXXXXXXXXX-378-174.jpg' />
+                      <Link to="Search" className='activity clearfix' href=''><img src='https://img.alicdn.com/tps/TB1xnkQKVXXXXaJXXXXXXXXXXXX-378-174.jpg' />
                         <div className='title'></div>
                         <div className='sub-title'></div>
-                      </a>
+                      </Link>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className='banner-con j_mainBanner loading' data-spm='2016006' style={{width: '1230px'}}>
-                <div className='banner-slider j_bannerSlider' style={{width: '1230px'}}>
+              <div className='banner-con j_mainBanner loading' data-spm='2016006' style={{ width: '1230px' }}>
+                <div className='banner-slider j_bannerSlider' style={{ width: '1230px' }}>
                   <Carousel autoplay effect='fade'>
                     <div>
                       <Link to='Course'>
-                      <img src='http://img.mukewang.com/5926501b000111f012000460.jpg' style={{width: '100%',height: '500px'}} />
+                        <img src='http://img.mukewang.com/5926501b000111f012000460.jpg' style={{ width: '100%', height: '500px' }} />
                       </Link>
                     </div>
                     <div>
                       <Link to='Course'>
-                      <img src='http://img.mukewang.com/592177a8000180ca12000460.jpg' style={{width: '100%',height: '500px'}} />
+                        <img src='http://img.mukewang.com/592177a8000180ca12000460.jpg' style={{ width: '100%', height: '500px' }} />
                       </Link>
                     </div>
                     <div>
                       <Link to='Course'>
-                      <img src='http://img.mukewang.com/59278d45000173ba12000460.jpg' style={{width: '100%',height: '500px'}} />
+                        <img src='http://img.mukewang.com/59278d45000173ba12000460.jpg' style={{ width: '100%', height: '500px' }} />
                       </Link>
                     </div>
                     <div>
                       <Link to='Course'>
-                      <img src='http://img.mukewang.com/591d825f00016a2612000460.jpg' style={{width: '100%',height: '500px'}} />
+                        <img src='http://img.mukewang.com/591d825f00016a2612000460.jpg' style={{ width: '100%', height: '500px' }} />
                       </Link>
                     </div>
                   </Carousel>
@@ -205,7 +251,7 @@ class IndexContent extends React.Component {
               </div>
             </div>
           </div>
-          <Activity2/>
+          <Activity2 />
         </div>
       </div>)
   }
--- src/router.js
+++ src/router.js
@@ -3,78 +3,106 @@ import { Router, Route, IndexRoute, Link } from 'dva/router';
 import IndexPage from './routes/IndexPage';
 
 const r11 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/1-1'))}, '11')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/1-1'))
+  }, '11')
 };
 const r12 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/1-2'))}, '12')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/1-2'))
+  }, '12')
 };
 const r21 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/2-1'))}, '21')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/2-1'))
+  }, '21')
 };
 const r22 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/2-2'))}, '22')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/2-2'))
+  }, '22')
 };
 const r31 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/3-1'))}, '31')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/3-1'))
+  }, '31')
 };
 const r32 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/3-2'))}, '32')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/3-2'))
+  }, '32')
 };
 const r33 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/3-3'))}, '33')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/3-3'))
+  }, '33')
 };
 const r34 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/3-4'))}, '34')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/3-4'))
+  }, '34')
 };
 const r35 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/login'))}, 'login')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/login'))
+  }, 'login')
 };
 const r36 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/Search'))}, 'Search')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/Search'))
+  }, 'Search')
 };
 const r37 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/Good'))}, 'Good')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/Good'))
+  }, 'Good')
 };
 const r38 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/School'))}, 'School')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/School'))
+  }, 'School')
 };
 const r39 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/Course'))}, 'Course')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/Course'))
+  }, 'Course')
 };
 const r40 = (location, callback) => {
-  require.ensure([], require => {callback(null,
-    require('./routes/Questions'))}, 'Questions')
+  require.ensure([], require => {
+    callback(null,
+      require('./routes/Questions'))
+  }, 'Questions')
 };
 
-export default function({ history }) {
+export default function ({ history }) {
   return (
     <Router hjsistory={history}>
       <Route path="/" component={IndexPage}>
- <Route path="Search" getComponent={r36} />
-<Route path="Good" getComponent={r37} />
-<Route path="School" getComponent={r38} />
-<Route path="Course" getComponent={r39} />
-<Route path="Questions" getComponent={r40} />
+        <Route path="Search" getComponent={r36} />
+        <Route path="Good" getComponent={r37} />
+        <Route path="School" getComponent={r38} />
+        <Route path="Course" getComponent={r39} />
+        <Route path="Questions" getComponent={r40} />
 
       </Route>
 
-    
-           
-      
-         <Route path="login" getComponent={r35} >
+
+
+
+      <Route path="login" getComponent={r35} >
 
         <Route path="12" getComponent={r12} />
         <Route path="21" getComponent={r21} />
@@ -83,10 +111,10 @@ export default function({ history }) {
         <Route path="32" getComponent={r32} />
         <Route path="33" getComponent={r33} />
         <Route path="34" getComponent={r34} />
-         <Route path="11" getComponent={r11} />
-</Route>
+        <Route path="11" getComponent={r11} />
+      </Route>
 
-  
+
     </Router>
   );
 };
