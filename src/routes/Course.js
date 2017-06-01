99185eae86426a94d835f7a00ed97758ddbde538
chscls <chscls@163.com>
2017-06-01 08:09:49 +0800

    111111

--- src/routes/Course.js
+++ src/routes/Course.js
@@ -25,7 +25,16 @@ return (
           <Col span={10}><div>
        
        
-        <VideoJs vtt="video/demo.captions.vtt2"/> </div>
+        <VideoJs vtt="video/demo.captions.vtt2"/> 
+        <p className="tm-action tm-clear">
+    <span id="J_EditItem"><a href="" target="_blank">举报</a></span>    			<a id="J_IShare" className="iShare tm-event" href="#"><Icon type="share-alt" />分享</a>
+				        <a id="J_AddFavorite" href="javascript:;" data-aldurl="//ald.taobao.com/recommend.htm?appId=03136&amp;itemId=544913594159 " className="favorite"><Icon type="star" /><span>收藏商品</span></a>
+		
+		
+				<span id="J_CollectCount">（50791人气）</span>    </p>
+        
+        
+        </div>
         
          </Col>
       <Col span={14}> <div>
@@ -77,7 +86,7 @@ Google亲儿子AndroidStudio即将上位，赶紧学习安装配置、使用技�
        
 			<dl className="tm-shopPromo-panel"><div className="tm-shopPromotion-title tm-gold">简介：Android Studio是Google在I/O大会上发布的一个新的集成开发环境，可以让Android开发变的更简单。本课程会详细的向您介绍Android Studio的安装配置、使用技巧以及相对于Eclipse开发的优势，并通过实际的操作让大家快速熟悉Android Studio的使用，让您体验更强大的开发工具</div></dl>
     
-<div className="tb-action tm-clear">
+<div className="tb-action tm-clear" style={{height:'95px'}}>
 																	                <div className="tb-btn-buy tb-btn-sku">
                     <a id="J_LinkBuy" href="#" rel="nofollow" data-addfastbuy="true" title="点击此按钮，到下一步确认购买信息。" role="button">立即学习<span className="ensureText">确认</span></a>
                 </div>
@@ -86,7 +95,26 @@ Google亲儿子AndroidStudio即将上位，赶紧学习安装配置、使用技�
         			</div>
 
        
-       
+       <div className="tm-ser tm-clear">
+                                                        	            <dl className="tm-clear">
+                                    <dt className="tb-metatit">服务承诺</dt>
+                                <dd className="tm-laysku-dd">
+                    <ul className="tb-serPromise"><li><a href="//www.tmall.com/wow/tmall-3c/act/new-serve?spm=a222t.8014944.rdfw.4.1VwWU1&amp;wh_isdetail=true&amp;wh_default_id=0&amp;wh_content_id=2&amp;wh_service_id=0" title="全国联保" target="_blank">全国联保</a></li><li><a href="//www.tmall.com/wow/portal/act/bzj" title="该商品由中国人保承保正品保证险" target="_blank">正品保证</a></li><li><a href="//www.tmall.com/wow/tmall-3c/act/new-serve?spm=a222t.8014944.fuxq1.12.sZdNvh&amp;wh_isdetail=true&amp;wh_service_id=3&amp;wh_content_id=1&amp;wh_default_id=1" title="小家电和数码产品质量问题365天只换不修" target="_blank">只换不修</a></li><li><a href="//vip.tmall.com/vip/privilege.htm?spm=3.1000588.0.141.2a0ae8&amp;priv=speed" title="极速退款是为诚信会员提供的退款退货流程的专享特权，额度是根据每个用户当前的信誉评级情况而定" target="_blank">极速退款</a></li><li><a href="//service.tmall.com/support/tmall/knowledge-1121473.htm?spm=0.0.0.0.asbDA1" title="卖家为您购买的商品投保退货运费险" target="_blank">赠运费险</a></li><li><a href="//pages.tmall.com/wow/seller/act/seven-day" title="七天无理由退换" target="_blank">七天无理由退换</a></li></ul>
+                </dd>
+            </dl>
+                                						            	<div className="tm-pay-box">
+                <div className="tm-pay">
+                    <em className="pay-title">支付方式</em>
+				                    <em title="显示所有信息" className="pay-toggler" id="J_Toggler" style={{display:'inline'}}></em>
+				                    <div data-spm="1998099674" className="pay-credit J_Paylist">
+																			                                <a title="支持使用信用卡支付" target="_blank" href="//payservice.alipay.com/intro/index.htm?c=xyk">信用卡<s style={{background:'url(//img.alicdn.com/tps/i4/TB1gNG3JpXXXXbcXVXXAz6UFXXX-16-16.png) no-repeat center center'}} ></s></a>
+														                                <a title="支持用绑定了支付宝的银行卡付款" href="//payservice.alipay.com/intro/index.htm?c=kjzf" target="_blank">快捷支付 <s style={{background:'url(//gtms04.alicdn.com/tps/i4/TB1fnvXJpXXXXcTXpXXAz6UFXXX-16-16.png) no-repeat center center'}}></s></a>
+														                                <a title="免费利用花呗额度支付，下月10号还款" href="//payservice.alipay.com/intro/index.htm?c=hb" target="_blank">支付宝 <s style={{background:'url(//gtms03.alicdn.com/tps/i3/TB1rf58JpXXXXc5XFXXAz6UFXXX-16-16.png) no-repeat center center'}}></s></a>
+																																										                                <a title="支持使用余额宝付款，边赚边花" href="//payservice.alipay.com/intro/index.htm?c=yeb" target="_blank">微信支付 <s style={{background:'url(//gtms02.alicdn.com/tps/i2/TB1HAHaJpXXXXX0XFXXAz6UFXXX-16-16.png) no-repeat center center'}}></s></a>
+													                    </div>
+                </div>
+            </div>
+						    </div>
         </div>
 
 
@@ -96,6 +124,12 @@ Google亲儿子AndroidStudio即将上位，赶紧学习安装配置、使用技�
 
 
   </Col>
+<Col>
+
+<div className="tm-layout"  data-spm="1998132085"><a href="//3c.tmall.com" target="_blank"><img style={{width:'955px'}} src="12.jpg"/></a></div>
+
+</Col>
+  
       </Row>
       </Col>
       <Col span={5}>
