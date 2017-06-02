3413af23ee9b433d57ef477ce05615d5ec6ebfa0
ChenHuan <chscls@163.com>
2017-06-02 10:46:41 +0800

    1

--- src/components/Silder.js
+++ src/components/Silder.js
@@ -24,23 +24,16 @@ const Sider = React.createClass({
         selectedKeys={[this.state.current]}
         mode="inline"
       >
-        <SubMenu key="sub1" title={<span><Icon type="mail" /><span>菜单一</span></span>}>
-          <Menu.Item key="1"><Link to="Member/11">菜单一1</Link></Menu.Item>
-          <Menu.Item key="2"><Link to="Member/12">菜单一2</Link></Menu.Item>
-            <SubMenu key="sub14" title={<span><Icon type="mail" /><span>菜单四</span></span>}>
-             <Menu.Item key="sub141"><Link to="Member/12">菜单四1</Link></Menu.Item>
-            </SubMenu>
+
+      <Menu.Item key="0"><Link to="Member/11">首页</Link></Menu.Item>
+      <Menu.Item key="10"><Link to="Member/11">我的课程</Link></Menu.Item>
+        
+        <SubMenu key="sub1" title={<span>我的收藏</span>}>
+          <Menu.Item key="1"><Link to="Member/11">收藏的课程</Link></Menu.Item>
+          <Menu.Item key="2"><Link to="Member/12">收藏的学校</Link></Menu.Item>
+        
         </SubMenu>
-        <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>菜单二</span></span>}>
-          <Menu.Item key="3"><Link to="Member/21">菜单二1</Link></Menu.Item>
-          <Menu.Item key="4"><Link to="Member/22">菜单二2</Link></Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub4" title={<span><Icon type="setting" /><span>菜单三</span></span>}>
-          <Menu.Item key="5"><Link to="Member/31">菜单三1</Link></Menu.Item>
-          <Menu.Item key="6"><Link to="Member/32">菜单三2</Link></Menu.Item>
-          <Menu.Item key="7"><Link to="Member/33">菜单三3</Link></Menu.Item>
-          <Menu.Item key="8"><Link to="Member/34">菜单三4</Link></Menu.Item>
-        </SubMenu>
+        
       </Menu>
     );
   },
