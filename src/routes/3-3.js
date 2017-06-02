e405fad8e571dace5e71633156ceecef975ed5d9
chscls <chscls@163.com>
2017-06-02 21:53:07 +0800

    1111111111

--- src/routes/3-3.js
+++ src/routes/3-3.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { connect } from 'dva';
+import AceEditor from 'react-ace';
+import brace from 'brace';
+
+ 
+import 'brace/mode/java';
+import 'brace/theme/tomorrow_night';
 class Option extends React.Component{
   constructor(props){
     super(props)
   }
+ onChange(newValue) {
+  console.log('change',newValue);
+}
   render(){
-    return (<div>菜单三3</div>)
+    var value="  public class HelloWorld { \r\n// 定义静态变量score1 \r\nstatic int score1 = 86; \r\n// 定义静态变量score2 \r\nint score2 = 92; \r\n// 定义静态方法sum，计算成绩总分，并返回总分 \r\npublic int sum() {\r\n } \r\npublic static void main(String[] args) { // 调用静态方法sum并接收返回值 \r\nint allScore = 100; \r\nSystem.out.println(&quot;总分：&quot; + allScore); \r\n} \r\n}"
+    return ( <AceEditor style={{width:'100%',minHeight:'800px'}}
+    mode="java"
+    theme="tomorrow_night"
+    onChange={this.onChange}
+    name="UNIQUE_ID_OF_DIV"
+    editorProps={{$blockScrolling: true}}
+    value={value}
+
+  /> )
   }
   componentDidMount(){
     const breadcrumbData = {
