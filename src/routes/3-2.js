bb430d67109ac5dcb6afd5c0476287757c44da0f
chscls <chscls@163.com>
2017-06-02 21:07:32 +0800

    11111111

--- src/routes/3-2.js
+++ src/routes/3-2.js
@@ -13,7 +13,7 @@ class Option extends React.Component{
       <Timeline pending={<a style={{ margin: 'auto',width:'100px'}} href="#">查看更多</a>}>
     <Timeline.Item dot={<Icon type="clock-circle-o" style={{ fontSize: '16px'}}/>}>
     
-       <div style={{width:'100%',height:'40px',float:'left',padding:'0px 5px'}}><h1>2016-07-01 至 2016-08-01</h1></div>
+       <div style={{width:'100%',height:'40px',padding:'0px 5px'}}><h1>2016-07-01 至 2016-08-01</h1></div>
     <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/><p style={{textAlign:'center'}}>学校名</p>  </div>
       <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/><p style={{textAlign:'center'}}>学校名</p>  </div>
       <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/><p style={{textAlign:'center'}}>学校名</p>  </div>
@@ -27,7 +27,7 @@ class Option extends React.Component{
       <div style={{clear:'both'}}></div>
       </Timeline.Item>
        <Timeline.Item style={{height:'240px'}}dot={<Icon type="clock-circle-o" style={{ fontSize: '16px'}}/>}>
-    <div style={{width:'100%',height:'40px',float:'left',padding:'0px 5px'}}><h1>2016-06-01 至 2016-07-01</h1></div>
+    <div style={{width:'100%',height:'40px',padding:'0px 5px'}}><h1>2016-06-01 至 2016-07-01</h1></div>
     <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/><p style={{textAlign:'center'}}>学校名</p>  </div>
       <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/><p style={{textAlign:'center'}}>学校名</p>  </div>
       <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/> <p style={{textAlign:'center'}}>学校名</p> </div>
@@ -36,7 +36,7 @@ class Option extends React.Component{
        <div style={{clear:'both'}}></div>
         </Timeline.Item>
       <Timeline.Item style={{height:'240px'}}dot={<Icon type="clock-circle-o" style={{ fontSize: '16px'}}/>}>
-    <div style={{width:'100%',height:'40px',float:'left',padding:'0px 5px'}}><h1>2016-05-01 至 2016-06-01</h1></div>
+    <div style={{width:'100%',height:'40px',padding:'0px 5px'}}><h1>2016-05-01 至 2016-06-01</h1></div>
     <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/> <p style={{textAlign:'center'}}>学校名</p></div>
       <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/><p style={{textAlign:'center'}}>学校名</p> </div>
       <div style={{width:'150px',height:'165px',float:'left',padding:'5px 5px'}}><img  src="http://iph.href.lu/140x140"/><p style={{textAlign:'center'}}>学校名</p> </div>
