9ae2b371698e3b6a87534e5286857372c520ceb8
ChenHuan <chscls@163.com>
2017-06-05 11:00:58 +0800

    1111

--- src/routes/CourseLearn.js
+++ src/routes/CourseLearn.js
@@ -1,15 +1,15 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'dva';
 import { Link } from 'dva/router';
-
-
+import Header from '../components/Header'
+import VideoJs from '../components/VideoJs'
 class CourseLearn extends Component {
   constructor(props) {
         super(props)
     }
 
      render() {
-        return (<div>课程学习页</div>);
+        return (<div><Header/><div style={{position:'absolute',top:'30',right:'0',left:'0',bottom:'0'}}><VideoJs/></div></div>);
      }
 }
 
