3ba539b28d6e77755663e8dd3d8288470af9433b
chscls <chscls@163.com>
2017-06-25 07:14:50 +0800

    11

--- src/components/WmcPosterForm.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { connect } from 'dva';
-import { Table, Icon } from 'antd';
-import { Input, Button, Slider,Modal, Form, Select, Upload, message } from 'antd';
-import {getImgUrl}from '../utils/query.js'
-import {rapHost} from '../config/config.js'
