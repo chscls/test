2f81f8c37d8e384fa5a196783ce012e8bfc92bd4
chscls <chscls@163.com>
2017-06-25 22:41:51 +0800

    1

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 const config = {
-  rapHost:'http://localhost:52059/soa/services',
+  rapHost:'http://eas.msvtc.net:52059/soa/services',
   rapFlag:true,
   onlinePath:'/soa/services/',
   onlineImagePath:"",
-  rapImagePath:"http://localhost:52059"
+  rapImagePath:"http://eas.msvtc.net:52059"
 }
 
 export default config
